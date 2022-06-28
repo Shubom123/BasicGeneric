0800ac34b8a230d3872c6d15511bcc87b8461c3c
Shubom123 <shubomnath365@gmail.com>
2022-06-28 19:10:14 +0530

    Added: Finding 1st, 2nd and 3rd max integer functionality added

--- GenericsMain.java
+++ GenericsMain.java
@@ -3,5 +3,29 @@ package com.generics;
 public class GenericsMain {
     public static void main(String[] args) {
         System.out.println("welcome to basics of generics");
+        int[] a = {5, 8, 6};
+        toSort(a);
+    }
+
+    private static void toSort(int[] a) {
+        int max=0;
+        int secondMax=0;
+        int thirdMax=0;
+        System.out.println("Integers before sorting" + a);
+        for(int i:a){
+            if(max<i){
+                thirdMax=secondMax;
+                secondMax=max;
+                max=i;
+            } else if (secondMax<i) {
+                thirdMax=secondMax;
+                secondMax=i;
+            } else {
+                thirdMax=i;
+            }
+        }
+        System.out.println("Max integer is: " + max);
+        System.out.println("Second max integer is: " + secondMax);
+        System.out.println("Third max integer is: " + thirdMax);
     }
 }
