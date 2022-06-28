22259155dba94662c89012257c0bcf095fccc1ab
Shubom123 <shubomnath365@gmail.com>
2022-06-28 20:19:10 +0530

    Added: Find max string functionality added

--- GenericsMain.java
+++ GenericsMain.java
@@ -5,11 +5,34 @@ public class GenericsMain {
         System.out.println("welcome to basics of generics");
         int[] a = {5, 8, 6};
         float[] b = {(float) 3.8, (float) 2.3, (float) 4.3};
+        String[] c = {"Shubom", "Anil", "Sumit"};
         toSort(a);
         toSort(b);
-
+        toSort(c);
     }
 
+    private static void toSort(String[] c) {
+        String max = " ";
+        String secondMax = " ";
+        String thirdMax = " ";
+        System.out.println("Float number after sorting");
+        for(String i:c){
+
+            if(max.length() < i.length()){
+                thirdMax=secondMax;
+                secondMax=max;
+                max=i;
+            } else if (secondMax.length() < i.length()) {
+                thirdMax=secondMax;
+                secondMax=i;
+            } else {
+                thirdMax=i;
+            }
+        }
+        System.out.println("Max float number is: " + max);
+        System.out.println("Second max float number is: " + secondMax);
+        System.out.println("Third max float number is: " + thirdMax);
+    }
 
     private static void toSort(float[] b) {
         float max=0;
