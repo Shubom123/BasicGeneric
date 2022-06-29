34c00dfdfe764649a181fcf7c83d391c894a3409
Shubom123 <shubomnath365@gmail.com>
2022-06-29 18:45:48 +0530

    Added: Second time refactoring done

--- GenericsMain.java
+++ GenericsMain.java
@@ -1,24 +1,33 @@
 package com.generics;
 
-public class GenericsMain {
+public class GenericsMain <T extends Comparable<T>> {
+    T x, y, z;
 
-    public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
-        T max = x;//assume x intially largest
-        if (y.compareTo(max) > 0) {
-            max = y;//y is the largest
+    public GenericsMain(T x,T y,T z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+
+    public T largest()  {
+
+        if (y.compareTo(x) > 0) {
+            return y;
         }
-        if (z.compareTo(max) > 0) {
-            max = z;//z is the lagest
+        if (z.compareTo(y) > 0) {
+            return z;
         } else {
-            max =x;
+            return x;
         }
-        return max;
     }
 
     public static void main(String[] args) {
-        System.out.println("Maximum number out of three integers is: " + maximum(11, 18, 200));
-        System.out.println("Maximum number out of three float is: " + maximum(4.3f, 0.75f, 88f));
-        System.out.println("Maximum among three string is: " + maximum("Shubom", "Anil", "Sumit"));
+        GenericsMain compareInt = new GenericsMain(11, 18, 200);
+        System.out.println("Maximum number out of three integers is: " + compareInt.largest());
+        GenericsMain compareFloat = new GenericsMain(4.3f, 0.75f, 88f);
+        System.out.println("Maximum number out of three float is: " + compareFloat.largest());
+        GenericsMain compareStr = new GenericsMain("Shubom", "Anil", "Sumit");
+        System.out.println("Maximum among three string is: " + compareStr.largest());
     }
 
 }
