0580e4184565b2c0e41acf91cfe5d35f44665fb2
Shubom123 <shubomnath365@gmail.com>
2022-06-29 19:52:59 +0530

    Added: Sorting functionality added

--- GenericsMain.java
+++ GenericsMain.java
@@ -1,33 +1,48 @@
 package com.generics;
+import java.util.Arrays;
 
 public class GenericsMain <T extends Comparable<T>> {
-    T x, y, z;
+    T[] inputArray;
 
-    public GenericsMain(T x,T y,T z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
+    public GenericsMain(T[] inputArray){
+        this.inputArray=inputArray;
+
     }
 
-    public T largest()  {
-
-        if (y.compareTo(x) > 0) {
-            return y;
+    public void largest()  {
+        for(int i=0; i<inputArray.length-1;i++){
+            if(inputArray[i].compareTo(inputArray[i+1])>0){
+                inputArray[i+1] = inputArray[i];
+            }
         }
-        if (z.compareTo(y) > 0) {
-            return z;
-        } else {
-            return x;
-        }
+        System.out.println(inputArray[inputArray.length - 1]);
     }
 
+    public void toSort(){
+
+        for(int i=0; i<inputArray.length;i++){
+            System.out.println(inputArray[i]);
+        }
+        Arrays.sort(inputArray);
+        System.out.println("After sorting");
+        for(int i=0; i<inputArray.length;i++) {
+            System.out.println(inputArray[i]);
+        }
+    }
     public static void main(String[] args) {
-        GenericsMain compareInt = new GenericsMain(11, 18, 200);
-        System.out.println("Maximum number out of three integers is: " + compareInt.largest());
-        GenericsMain compareFloat = new GenericsMain(4.3f, 0.75f, 88f);
-        System.out.println("Maximum number out of three float is: " + compareFloat.largest());
-        GenericsMain compareStr = new GenericsMain("Shubom", "Anil", "Sumit");
-        System.out.println("Maximum among three string is: " + compareStr.largest());
+        System.out.println("**************");
+        Integer[] I = {56, 30, 70, 123, 43};
+        System.out.println("**************");
+        String[] S = {"Shubom", "Supali", "Jyoti", "Smriti","Mishon"};
+        Float[] F = {0.56f, 000000.30f, 7.340f, 0000.01f};
+        System.out.println("*******Array Sorting*******");
+        new GenericsMain(I).toSort();
+        new GenericsMain(S).toSort();
+        new GenericsMain(F).toSort();
+        System.out.println("*******Finding Maximum*******");
+        new GenericsMain(I).largest();
+        new GenericsMain(S).largest();
+        new GenericsMain(F).largest();
     }
 
 }
